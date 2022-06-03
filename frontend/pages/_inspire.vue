3710f1497b643a03695ec88c2bca0342c3bf70b5
BenJacquet <benjamin.jacquet@live.fr>
2022-06-03 15:33:52 +0200

    added components

--- frontend/pages/_inspire.vue
+++ frontend/pages/_inspire.vue
@@ -1,6 +1,8 @@
 <template>
+<v-app>
   <v-row>
     <v-col class="text-center">
+		<v-btn color="secondary">42 Connect</v-btn>
       <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
@@ -12,10 +14,17 @@
       </blockquote>
     </v-col>
   </v-row>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'InspirePage',
+	theme: {dark: true},
 }
 </script>
+
+<style scoped>
+.v-application {
+  background: linear-gradient(180deg, #050E13 0%, #113143 41.15%, #1C5474 81.77%);
+}
+</style>
