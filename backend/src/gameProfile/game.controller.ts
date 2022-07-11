6de57b0fbaa39478ceed589d9ba35d2961c9cb82
lucocozz <lucocozz@student.42.fr>
2022-07-11 03:49:04 +0200

    api: adding setter for game controller

--- backend/src/gameProfile/game.controller.ts
+++ backend/src/gameProfile/game.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Query, Res } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { GameService } from './game.service';
 import { Response } from 'express';
-import { Party } from '@prisma/client';
+import { GameProfile, Party } from '@prisma/client';
 
 @ApiTags('game')
 @Controller('game')
@@ -13,7 +13,7 @@ export class GameController {
   async getGameProfile(
     @Res() res: Response,
     @Param('id') id: number,
-  ): Promise<any> {
+  ): Promise<GameProfile> {
     return await this.gameService.getGameProfile(res, Number(id));
   }
 
@@ -22,7 +22,34 @@ export class GameController {
     @Res() res: Response,
     @Param('id') id: number,
   ): Promise<string | null> {
-    return await this.gameService.getLevel(res, Number(id));
+    return await this.gameService.getLevel(Number(id), res);
+  }
+
+  @Patch(':id/level/set/:level')
+  async setLevel(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Param('level') level: number,
+  ): Promise<number | undefined> {
+    return await this.gameService.setLevel(Number(id), Number(level), res);
+  }
+
+  @Patch(':id/level/add/:value')
+  async addLevel(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Param('value') value: number,
+  ): Promise<number | undefined> {
+    return await this.gameService.addLevel(Number(id), Number(value), res);
+  }
+
+  @Patch(':id/level/remove/:value')
+  async removeLevel(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Param('value') value: number,
+  ): Promise<number | undefined> {
+    return await this.gameService.removeLevel(Number(id), Number(value), res);
   }
 
   @Get(':id/xp')
@@ -33,12 +60,84 @@ export class GameController {
     return await this.gameService.getXp(res, Number(id));
   }
 
+  @Patch(':id/xp/set/:xp')
+  async setXp(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Param('xp') xp: number,
+  ): Promise<number | undefined> {
+    return await this.gameService.setXp(Number(id), Number(xp), res);
+  }
+
+  @Patch(':id/xp/add/:value')
+  async addXp(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Param('value') value: number,
+  ): Promise<number | undefined> {
+    return await this.gameService.addXp(Number(id), Number(value), res);
+  }
+
+  @Patch(':id/xp/remove/:value')
+  async removeXp(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Param('value') value: number,
+  ): Promise<number | undefined> {
+    return await this.gameService.removeXp(Number(id), Number(value), res);
+  }
+
   @Get(':id/nbParty')
   async getNbParty(
     @Res() res: Response,
     @Param('id') id: number,
   ): Promise<string | null> {
     return await this.gameService.getNbParty(res, Number(id));
+  }
+
+  @Patch(':id/nbParty/set/:nbParty')
+  async setNbParty(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Param('nbParty') nbParty: number,
+    @Query('win') win: string,
+  ): Promise<number | undefined> {
+    return await this.gameService.setNbParty(
+      Number(id),
+      Number(nbParty),
+      win,
+      res,
+    );
+  }
+
+  @Patch(':id/nbParty/add/:value')
+  async addNbParty(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Param('value') value: number,
+    @Query('win') win: string,
+  ): Promise<number | undefined> {
+    return await this.gameService.addNbParty(
+      Number(id),
+      Number(value),
+      win,
+      res,
+    );
+  }
+
+  @Patch(':id/nbParty/remove/:value')
+  async removeNbParty(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Param('value') value: number,
+    @Query('win') win: string,
+  ): Promise<number | undefined> {
+    return await this.gameService.removeNbParty(
+      Number(id),
+      Number(value),
+      win,
+      res,
+    );
   }
 
   @Get(':id/nbWin')
@@ -62,6 +161,45 @@ export class GameController {
     @Res() res: Response,
     @Param('id') id: number,
   ): Promise<Party[] | null> {
-    return await this.gameService.getHistory(res, Number(id));
+    return await this.gameService.getHistory(Number(id), res);
+  }
+
+  @Get(':id/history/:partyId')
+  async getHistoryParty(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Param('partyId') partyId: number,
+  ): Promise<Party | null> {
+    return await this.gameService.getHistoryParty(
+      res,
+      Number(id),
+      Number(partyId),
+    );
+  }
+
+  @Patch(':id/history/add/:partyId')
+  async addToHistory(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Param('partyId') partyId: number,
+  ): Promise<Party[] | null> {
+    return await this.gameService.addToHistory(
+      Number(id),
+      Number(partyId),
+      res,
+    );
+  }
+
+  @Patch(':id/history/remove/:partyId')
+  async removeFromHistory(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Param('partyId') partyId: number,
+  ): Promise<Party[] | null> {
+    return await this.gameService.removeFromHistory(
+      Number(id),
+      Number(partyId),
+      res,
+    );
   }
 }
--- backend/src/gameProfile/game.service.ts
+++ backend/src/gameProfile/game.service.ts
@@ -7,6 +7,17 @@ import { Response } from 'express';
 export class GameService {
   constructor(private prisma: PrismaService) {}
 
+  toBool(str: string): boolean {
+    if (str === null || str === undefined) return undefined;
+    str = str.toLowerCase();
+    if (['true', '1', 'on', 'yes'].includes(str)) {
+      return true;
+    } else if (['false', '0', 'off', 'no'].includes(str)) {
+      return false;
+    }
+    return undefined;
+  }
+
   async getGameProfile(res: Response, id: number): Promise<GameProfile | null> {
     try {
       const profile = await this.prisma.gameProfile.findUnique({
@@ -20,7 +31,7 @@ export class GameService {
     }
   }
 
-  async getLevel(res: Response, id: number): Promise<string | null> {
+  async getLevel(id: number, res?: Response): Promise<string | null> {
     try {
       const profile = await this.prisma.gameProfile.findUniqueOrThrow({
         where: { UserId: id },
@@ -30,6 +41,72 @@ export class GameService {
     } catch (error) {
       res.status(HttpStatus.NOT_FOUND).send('User not found');
       return null;
+    }
+  }
+
+  async setLevel(
+    id: number,
+    level: number,
+    res?: Response,
+  ): Promise<number | undefined> {
+    try {
+      await this.prisma.gameProfile.update({
+        where: { UserId: id },
+        data: { Level: level },
+      });
+      if (res !== undefined) {
+        res.status(HttpStatus.OK).send(level.toString());
+      }
+      return level;
+    } catch (error) {
+      if (res !== undefined) {
+        res.status(HttpStatus.NOT_FOUND).send(error);
+      }
+      return undefined;
+    }
+  }
+
+  async addLevel(
+    id: number,
+    value: number,
+    res?: Response,
+  ): Promise<number | undefined> {
+    try {
+      const profile = await this.prisma.gameProfile.update({
+        where: { UserId: id },
+        data: { Level: { increment: value } },
+      });
+      if (res !== undefined) {
+        res.status(HttpStatus.OK).send(profile.Level.toString());
+      }
+      return profile.Level;
+    } catch (error) {
+      if (res !== undefined) {
+        res.status(HttpStatus.NOT_FOUND).send(error);
+      }
+      return undefined;
+    }
+  }
+
+  async removeLevel(
+    id: number,
+    value: number,
+    res?: Response,
+  ): Promise<number | undefined> {
+    try {
+      const profile = await this.prisma.gameProfile.update({
+        where: { UserId: id },
+        data: { Level: { decrement: value } },
+      });
+      if (res !== undefined) {
+        res.status(HttpStatus.OK).send(profile.Level.toString());
+      }
+      return profile.Level;
+    } catch (error) {
+      if (res !== undefined) {
+        res.status(HttpStatus.NOT_FOUND).send(error);
+      }
+      return undefined;
     }
   }
 
@@ -46,6 +123,72 @@ export class GameService {
     }
   }
 
+  async setXp(
+    id: number,
+    xp: number,
+    res?: Response,
+  ): Promise<number | undefined> {
+    try {
+      await this.prisma.gameProfile.update({
+        where: { UserId: id },
+        data: { Xp: xp },
+      });
+      if (res !== undefined) {
+        res.status(HttpStatus.OK).send(xp.toString());
+      }
+      return xp;
+    } catch (error) {
+      if (res !== undefined) {
+        res.status(HttpStatus.NOT_FOUND).send(error);
+      }
+      return undefined;
+    }
+  }
+
+  async addXp(
+    id: number,
+    value: number,
+    res?: Response,
+  ): Promise<number | undefined> {
+    try {
+      const profile = await this.prisma.gameProfile.update({
+        where: { UserId: id },
+        data: { Xp: { increment: value } },
+      });
+      if (res !== undefined) {
+        res.status(HttpStatus.OK).send(profile.Xp.toString());
+      }
+      return profile.Xp;
+    } catch (error) {
+      if (res !== undefined) {
+        res.status(HttpStatus.NOT_FOUND).send(error);
+      }
+      return undefined;
+    }
+  }
+
+  async removeXp(
+    id: number,
+    value: number,
+    res?: Response,
+  ): Promise<number | undefined> {
+    try {
+      const profile = await this.prisma.gameProfile.update({
+        where: { UserId: id },
+        data: { Xp: { decrement: value } },
+      });
+      if (res !== undefined) {
+        res.status(HttpStatus.OK).send(profile.Xp.toString());
+      }
+      return profile.Xp;
+    } catch (error) {
+      if (res !== undefined) {
+        res.status(HttpStatus.NOT_FOUND).send(error);
+      }
+      return undefined;
+    }
+  }
+
   async getNbParty(res: Response, id: number): Promise<string | null> {
     try {
       const profile = await this.prisma.gameProfile.findUniqueOrThrow({
@@ -56,6 +199,84 @@ export class GameService {
     } catch (error) {
       res.status(HttpStatus.NOT_FOUND).send('User not found');
       return null;
+    }
+  }
+
+  async setNbParty(
+    id: number,
+    nbParty: number,
+    win: string,
+    res?: Response,
+  ): Promise<number | undefined> {
+    try {
+      await this.prisma.gameProfile.update({
+        where: { UserId: id },
+        data: {
+          NbParty: nbParty,
+          NbWin: this.toBool(win) === true ? nbParty : undefined,
+        },
+      });
+      if (res !== undefined) {
+        res.status(HttpStatus.OK).send(nbParty.toString());
+      }
+      return nbParty;
+    } catch (error) {
+      if (res !== undefined) {
+        res.status(HttpStatus.NOT_FOUND).send(error);
+      }
+      return undefined;
+    }
+  }
+
+  async addNbParty(
+    id: number,
+    value: number,
+    win: string,
+    res?: Response,
+  ): Promise<number | undefined> {
+    try {
+      const profile = await this.prisma.gameProfile.update({
+        where: { UserId: id },
+        data: {
+          NbParty: { increment: value },
+          NbWin: this.toBool(win) === true ? value : 0,
+        },
+      });
+      if (res !== undefined) {
+        res.status(HttpStatus.OK).send(profile.NbParty.toString());
+      }
+      return profile.NbParty;
+    } catch (error) {
+      if (res !== undefined) {
+        res.status(HttpStatus.NOT_FOUND).send(error);
+      }
+      return undefined;
+    }
+  }
+
+  async removeNbParty(
+    id: number,
+    value: number,
+    win: string,
+    res?: Response,
+  ): Promise<number | undefined> {
+    try {
+      const profile = await this.prisma.gameProfile.update({
+        where: { UserId: id },
+        data: {
+          NbParty: { decrement: value },
+          NbWin: { decrement: this.toBool(win) === true ? value : 0 },
+        },
+      });
+      if (res !== undefined) {
+        res.status(HttpStatus.OK).send(profile.NbParty.toString());
+      }
+      return profile.NbParty;
+    } catch (error) {
+      if (res !== undefined) {
+        res.status(HttpStatus.NOT_FOUND).send(error);
+      }
+      return undefined;
     }
   }
 
@@ -87,7 +308,7 @@ export class GameService {
     }
   }
 
-  async getHistory(res: Response, id: number): Promise<Party[] | null> {
+  async getHistory(id: number, res?: Response): Promise<Party[] | null> {
     try {
       const profile = await this.prisma.gameProfile.findUniqueOrThrow({
         where: { UserId: id },
@@ -97,6 +318,72 @@ export class GameService {
       return profile.History;
     } catch (error) {
       res.status(HttpStatus.NOT_FOUND).send('User not found');
+      return null;
+    }
+  }
+
+  async getHistoryParty(
+    res: Response,
+    id: number,
+    idParty,
+  ): Promise<Party | null> {
+    try {
+      const history = await this.getHistory(id);
+      const party = history.find((p) => p.id === idParty);
+      if (party === undefined) {
+        res.status(HttpStatus.NOT_FOUND).send('Party not found');
+        return null;
+      }
+      res.status(HttpStatus.OK).send(party);
+      return party;
+    } catch (error) {
+      res.status(HttpStatus.NOT_FOUND).send('User not found');
+      return null;
+    }
+  }
+
+  async addToHistory(
+    id: number,
+    idParty: number,
+    res?: Response,
+  ): Promise<Party[] | null> {
+    try {
+      const profile = await this.prisma.gameProfile.update({
+        where: { UserId: id },
+        data: { History: { connect: { id: idParty } } },
+        include: { History: true },
+      });
+      if (res !== undefined) {
+        res.status(HttpStatus.OK).send(profile.History);
+      }
+      return profile.History;
+    } catch (error) {
+      if (res !== undefined) {
+        res.status(HttpStatus.NOT_FOUND).send(error);
+      }
+      return null;
+    }
+  }
+
+  async removeFromHistory(
+    id: number,
+    idParty: number,
+    res?: Response,
+  ): Promise<Party[] | null> {
+    try {
+      const profile = await this.prisma.gameProfile.update({
+        where: { UserId: id },
+        data: { History: { disconnect: { id: idParty } } },
+        include: { History: true },
+      });
+      if (res !== undefined) {
+        res.status(HttpStatus.OK).send(profile.History);
+      }
+      return profile.History;
+    } catch (error) {
+      if (res !== undefined) {
+        res.status(HttpStatus.NOT_FOUND).send(error);
+      }
       return null;
     }
   }
