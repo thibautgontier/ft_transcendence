61895ebf19d61e9d271f9dbf1e3bd00266f3691c
mbaxmann <mathias.bamann4@gmail.com>
2022-08-25 15:23:17 +0200

    redirection bugfix + small login visual feedback

--- frontend/components/Login.vue
+++ frontend/components/Login.vue
@@ -2,7 +2,8 @@
 	export default {
 		data() {
 			return {
-				password: "init"
+				password: "init",
+				isConnected: 0
 				}
 		},
 		head: {
@@ -10,7 +11,10 @@
 		},
 		methods: {
 			redirectToLog() {
-				window.location.href = "login-main"
+				window.location.href = "http://localhost:3000/login-main"
+			},
+			connectTest() {
+				this.isConnected = 1
 			},
 		}
 
@@ -19,13 +23,24 @@
 
 <template>
 	<v-container fill-height>
+	<h1 v-if="isConnected">
+	<Toolbar />
+	</h1>
 		<v-col>
 			<v-row justify="center" align="center">
 				<PongLogo />
 			</v-row>
 			<v-row justify="center" align="center" style="margin-top: 10%">
 			<v-btn x-large color="black" @click.stop="redirectToLog()">42 Connect</v-btn>
+			<v-btn x-large color="black" @click.stop="connectTest()">TEST</v-btn>
 			</v-row>
+			<h1 v-if="isConnected">
+				<v-alert
+					type="success"
+					transition="scale-transition"
+					dismissible
+				>Logged successfully!</v-alert>
+			</h1>
 		</v-col>
 	</v-container>
 </template>
