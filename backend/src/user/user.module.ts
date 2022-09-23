8f789ba0f429f0161e9a1cbc1d42300888b2a776
thibautgontier <thibautgontier@gmail.com>
2022-09-23 12:06:18 +0200

    fixed the user module

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { AuthModule } from 'src/auth/auth.module';
+import { AuthService } from 'src/auth/auth.service';
 import { SessionSerializer } from 'src/auth/session.serializer';
 import { FtStrategy } from 'src/auth/strategies/ft.strategy';
 import { PrismaController } from 'src/prisma/prisma.controller';
@@ -7,7 +10,15 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [UserController, PrismaController],
-  providers: [UserService, PrismaService, FtStrategy, SessionSerializer],
+  providers: [
+    UserService,
+    PrismaService,
+    FtStrategy,
+    SessionSerializer,
+    AuthService,
+    JwtService,
+  ],
 })
 export class UserModule {}
